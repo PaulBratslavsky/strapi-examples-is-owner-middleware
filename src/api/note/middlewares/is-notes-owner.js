193a18a225e2fa8e26213a6cc81e4077159b2033
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2023-11-14 08:15:58 -0600

    update and refactor

--- src/api/note/middlewares/is-notes-owner.js
+++ src/api/note/middlewares/is-notes-owner.js
@@ -5,37 +5,50 @@
  */
 
 module.exports = (config, { strapi }) => {
-  // Add your own logic here.
   return async (ctx, next) => {
     strapi.log.info("In is-note-owner middleware.");
+    
+    // Extract entryId and user from context
+    const entryId = ctx.params.id;
     const user = ctx.state.user;
-    const userId = user.id;
+    const userId = user?.id;
 
     // This query allows user to populate the fields and relations
     // if you need additional security you would update the query accordingly
     // and remove ...ctx.query
-    
-    ctx.query = {
-      ...ctx.query,
-      filters: {
-        author: userId,
-      },
-    };
 
-    const entryId = ctx.params.id ? ctx.params.id : undefined;
+    // If no user is logged in, adjust the query for public notes
+    if (!user) {
+      strapi.log.info("No user, Public notes only.");
+      ctx.query = {
+        ...ctx.query,
+        filters: { ...ctx.query.filters, isPublic: true },
+      };
 
-    let entry = null;
+      // Check if the specific entry is public
+      if (entryId) {
+        const entry = await strapi.entityService.findOne("api::note.note", entryId, { populate: "*" });
+        if (entry && !entry.isPublic) {
+          return ctx.unauthorized(`You can't access this entry`);
+        }
+      }
+    } else {
+      // Update query filters for authenticated user
+      ctx.query = {
+        ...ctx.query,
+        filters: { ...ctx.query.filters, author: userId },
+      };
 
-    if (entryId) {
-      entry = await strapi.entityService.findOne("api::note.note", entryId, {
-        populate: "*",
-      });
+      // Check if the user owns the specific entry
+      if (entryId) {
+        const entry = await strapi.entityService.findOne("api::note.note", entryId, { populate: "*" });
+        if (entry && entry.author.id !== userId) {
+          return ctx.unauthorized(`You can't access this entry`);
+        }
+      }
     }
 
-    if (entry && entry.author.id !== userId) {
-      return ctx.unauthorized(`You can't access this entry`);
-    }
-
+    // Proceed to the next middleware
     await next();
   };
 };
